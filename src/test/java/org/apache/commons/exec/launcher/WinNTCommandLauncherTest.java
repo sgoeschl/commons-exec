effb7867d62d049effb7ecdb3c2b28ca335a5968
Siegfried Goeschl <siegfried.goeschl@it20one.at>
2016-01-11 21:30:14 +0100

    [EXEC-95] Add missing licence header

--- src/test/java/org/apache/commons/exec/launcher/WinNTCommandLauncherTest.java
+++ src/test/java/org/apache/commons/exec/launcher/WinNTCommandLauncherTest.java
@@ -1,20 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
 package org.apache.commons.exec.launcher;
-
-import org.apache.commons.exec.CommandLine;
-import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
+import org.apache.commons.exec.CommandLine;
+import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class WinNTCommandLauncherTest {
 
     private static final String ANY_EXECUTABLE_NAME = "test.bat";
     private static final File ANY_WORKING_DIR = new File("/");
-    private CommandLauncherStub commandLauncherStub = new CommandLauncherStub();
-    private WinNTCommandLauncher winNTCommandLauncher = new WinNTCommandLauncher(commandLauncherStub);
+
+    private CommandLauncherStub commandLauncher = new CommandLauncherStub();
+    private WinNTCommandLauncher winNTCommandLauncher = new WinNTCommandLauncher(commandLauncher);
 
     @Test
     public void testWinNTCommandLineWithoutWorkingDirectory() throws Exception {
@@ -22,8 +39,8 @@ public class WinNTCommandLauncherTest {
 
         winNTCommandLauncher.exec(cmd, null, null);
 
-        assertEquals(1, commandLauncherStub.cmd.toStrings().length);
-        assertEquals(ANY_EXECUTABLE_NAME, commandLauncherStub.cmd.getExecutable());
+        assertEquals(1, commandLauncher.cmd.toStrings().length);
+        assertEquals(ANY_EXECUTABLE_NAME, commandLauncher.cmd.getExecutable());
     }
 
     @Test
@@ -32,14 +49,14 @@ public class WinNTCommandLauncherTest {
 
         winNTCommandLauncher.exec(cmd, null, ANY_WORKING_DIR);
 
-        assertEquals(7, commandLauncherStub.cmd.toStrings().length);
-        assertEquals("cmd", commandLauncherStub.cmd.getExecutable());
-        assertEquals("/c", commandLauncherStub.cmd.getArguments()[0]);
-        assertEquals("cd", commandLauncherStub.cmd.getArguments()[1]);
-        assertEquals("/d", commandLauncherStub.cmd.getArguments()[2]);
-        assertEquals(ANY_WORKING_DIR.getAbsolutePath(), commandLauncherStub.cmd.getArguments()[3]);
-        assertEquals("&&", commandLauncherStub.cmd.getArguments()[4]);
-        assertEquals(ANY_EXECUTABLE_NAME, commandLauncherStub.cmd.getArguments()[5]);
+        assertEquals(7, commandLauncher.cmd.toStrings().length);
+        assertEquals("cmd", commandLauncher.cmd.getExecutable());
+        assertEquals("/c", commandLauncher.cmd.getArguments()[0]);
+        assertEquals("cd", commandLauncher.cmd.getArguments()[1]);
+        assertEquals("/d", commandLauncher.cmd.getArguments()[2]);
+        assertEquals(ANY_WORKING_DIR.getAbsolutePath(), commandLauncher.cmd.getArguments()[3]);
+        assertEquals("&&", commandLauncher.cmd.getArguments()[4]);
+        assertEquals(ANY_EXECUTABLE_NAME, commandLauncher.cmd.getArguments()[5]);
     }
 
     private static final class CommandLauncherStub implements CommandLauncher {
